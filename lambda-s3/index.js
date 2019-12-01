d9ff92f760e194bc473e7084f6cc8c632350c395
Scott Quach <scottqglobal@gmail.com>
2019-12-01 13:02:48 -0800

    S3 signed url integration

--- lambda-s3/index.js
+++ lambda-s3/index.js
@@ -1,21 +1,33 @@
-// const AWS = require('aws-sdk');
+const AWS = require('aws-sdk');
 
-// AWS.config.update({
-//     region: 'us-west-2'
-// });
+AWS.config.update({
+    region: 'us-west-2'
+});
 
-// const S3 = new AWS.S3();
+const S3 = new AWS.S3();
 
 exports.handler = async event => {
     // TODO implement
     console.log(event);
-    // S3.getSignedUrlPromise('getObject', {
-    //     Bucket: 'bucket',
-    //     Key: 'key'
-    // })
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify(event)
-    };
-    return response;
+    return S3.getSignedUrlPromise('getObject', {
+        Bucket: 'virtual-tour-storage',
+        Key: 't-from-t-mobile-logo-386329.png'
+    })
+        .then(result => {
+            console.log(result);
+            const response = {
+                statusCode: 200,
+                body: {
+                    signedUrl: result
+                }
+            };
+            return response;
+        })
+        .catch(error => {
+            const response = {
+                statusCode: 500,
+                body: error
+            };
+            return response;
+        });
 };
