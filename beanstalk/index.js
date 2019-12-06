c987ba11aa54efd0809a5bb3c00a2551c84f11c0
Scott Quach <scottqglobal@gmail.com>
2019-12-05 21:18:28 -0800

    Lambda function can be invoked via api

--- beanstalk/index.js
+++ beanstalk/index.js
@@ -1,9 +1,49 @@
 const express = require('express');
 const app = express();
-
 const history = require('connect-history-api-fallback');
+const AWS = require('aws-sdk');
+const lambda = new AWS.Lambda({
+    region: 'us-west-2'
+});
 
 const port = process.env.PORT || 3000;
+
+app.get('/api/album', (request, response) => {
+    const params = {
+        FunctionName: 'GetAlbumImages',
+        InvocationType: 'RequestResponse',
+        Payload: JSON.stringify({
+            AlbumID: 3
+        })
+    };
+
+    lambda
+        .invoke(params)
+        .promise()
+        .then(result => {
+            response.send(result);
+        })
+        .catch(error => {
+            response.send(error);
+        });
+});
+
+app.get('/api/albums', (request, response) => {
+    const params = {
+        FunctionName: 'GetAlbumsList',
+        InvocationType: 'RequestResponse'
+    };
+
+    lambda
+        .invoke(params)
+        .promise()
+        .then(result => {
+            response.send(result);
+        })
+        .catch(error => {
+            response.send(error);
+        });
+});
 
 app.use(history());
 app.use(express.static('./dist'));
